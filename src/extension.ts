4e28037ea101a8eb3f26f1801f4e50b90875e04d
Fred Bricon <fbricon@gmail.com>
2021-03-15 18:13:07 +0100

    Update to latest vscode-kafka API
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,99 +1,108 @@
 import axios from 'axios';
 import * as vscode from 'vscode';
-import { Cluster, ClusterProviderProcessor, ClusterSettings, ConnectionOptions, KafkaConfig } from './vscodekafka-api'
-
+import { Cluster, KafkaExtensionParticipant, ClusterSettings, ConnectionOptions, KafkaConfig, ClusterProviderParticipant } from './vscodekafka-api';
+​
 import { getTelemetryService, TelemetryService } from "@redhat-developer/vscode-redhat-telemetry";
-
+​
 const KAFKA_API = 'https://api.stage.openshift.com/api/managed-services-api/v1/kafkas';
 const LANDING_PAGE = 'https://cloud.redhat.com/beta/application-services/openshift-streams';
-
-
-export async function activate(_context: vscode.ExtensionContext): Promise<ClusterProviderProcessor> {
-	let telemetryService: TelemetryService = await getTelemetryService("redhat.vscode-rhoas")
+​
+export async function activate(_context: vscode.ExtensionContext): Promise<KafkaExtensionParticipant> {
+	let telemetryService: TelemetryService = await getTelemetryService("redhat.vscode-rhoas");
 	telemetryService.sendStartupEvent();
 	return getRHOSAKClusterProvider(telemetryService);
 }
-
-const RHOSAK_CLUSTER_PROVIDER_ID =  "rhosak";
+​
+const RHOSAK_CLUSTER_PROVIDER_ID = "rhosak";
 const RHOSAK_LABEL = "Red Hat OpenShift Streams for Apache Kafka";
-
-function getRHOSAKClusterProvider(telemetryService: TelemetryService): ClusterProviderProcessor {
+​
+function getRHOSAKClusterProvider(telemetryService: TelemetryService): KafkaExtensionParticipant {
 	return {
-		async collectClusters(clusterSettings: ClusterSettings): Promise<Cluster[] | undefined> {
-			const session = await vscode.authentication.getSession('redhat-account-auth', ['openid'], { createIfNone: true });
-			if (!session) {
-				vscode.window.showWarningMessage('You need to log into Red Hat first!');
-				return [];
-			}
-			const start = new Date().getTime();
-			const existingClusters = clusterSettings.getAll().map(cluster => cluster.bootstrap);
-			let clusters = await vscode.window.withProgress({
-				location: vscode.ProgressLocation.Notification,
-			  }, async (progress) => {
-				progress.report({
-					message: `Fetching Kafka cluster definitions from Red Hat...`,
-				});
-				return getRHOSAKClusters(session.accessToken);
-			});
-			const foundServers = clusters.length > 0;
-			if (foundServers && existingClusters.length > 0) {
-				clusters = clusters.filter( mk => !existingClusters.includes(mk.bootstrap));
-			}
-			if (clusters.length > 0) {
-				// preemptively sign into MAS SSO, so the 2 sign-ins are chained, which makes things ... less awkward? really?
-				await vscode.authentication.getSession('redhat-mas-account-auth', ['openid'], { createIfNone: true });
-				let event = {
-					name: "add_rhosak_clusters",
-					properties: {
-						"clusters": clusters.length
-					}
-				};
-				telemetryService.send(event);
-				return clusters;
-			} else if (foundServers) {
-				vscode.window.showInformationMessage(`All ${RHOSAK_LABEL} Clusters have already been added`);
-			} else {
-				// Should open the landing page
-				vscode.window.showWarningMessage(`No ${RHOSAK_LABEL} cluster available! Visit ${LANDING_PAGE}`);
-			}
-			return [];
-		},
-		createKafkaConfig(connectionOptions: ConnectionOptions): KafkaConfig {
+		getClusterProviderParticipant(clusterProviderId: string): ClusterProviderParticipant {
 			return {
-				clientId: "vscode-kafka",
-				brokers: connectionOptions.bootstrap.split(","),
-				ssl: true,
-				sasl: { 
-					mechanism:'oauthbearer', 
-					oauthBearerProvider: async () => {
-						const session = await vscode.authentication.getSession('redhat-mas-account-auth',['openid'], { createIfNone: true});
-						const token = session?.accessToken!;
-						return {
-							value: token
-						};
-					}
-				}
-			};
+				configureClusters: async (clusterSettings: ClusterSettings): Promise<Cluster[] | undefined> => configureClusters(clusterSettings, telemetryService),
+				createKafkaConfig: (connectionOptions: ConnectionOptions): KafkaConfig => createKafkaConfig(connectionOptions)
+			} as ClusterProviderParticipant;
 		}
 	};
 }
-
+​
+async function configureClusters(clusterSettings: ClusterSettings, telemetryService: TelemetryService): Promise<Cluster[] | undefined> {
+	const session = await vscode.authentication.getSession('redhat-account-auth', ['openid'], { createIfNone: true });
+	if (!session) {
+		vscode.window.showWarningMessage('You need to log into Red Hat first!');
+		return [];
+	}
+	const start = new Date().getTime();
+	const existingClusters = clusterSettings.getAll().map(cluster => cluster.bootstrap);
+	let clusters = await vscode.window.withProgress({
+		location: vscode.ProgressLocation.Notification,
+	}, async (progress) => {
+		progress.report({
+			message: `Fetching Kafka cluster definitions from Red Hat...`,
+		});
+		return getRHOSAKClusters(session.accessToken);
+	});
+	const foundServers = clusters.length > 0;
+	if (foundServers && existingClusters.length > 0) {
+		clusters = clusters.filter(mk => !existingClusters.includes(mk.bootstrap));
+	}
+	if (clusters.length > 0) {
+		// preemptively sign into MAS SSO, so the 2 sign-ins are chained, which makes things ... less awkward? really?
+		await vscode.authentication.getSession('redhat-mas-account-auth', ['openid'], { createIfNone: true });
+		let event = {
+			name: "add_rhosak_clusters",
+			properties: {
+				"clusters": clusters.length
+			}
+		};
+		telemetryService.send(event);
+		return clusters;
+	} else if (foundServers) {
+		vscode.window.showInformationMessage(`All ${RHOSAK_LABEL} Clusters have already been added`);
+	} else {
+		// Should open the landing page
+		vscode.window.showWarningMessage(`No ${RHOSAK_LABEL} cluster available! Visit ${LANDING_PAGE}`);
+	}
+	return [];
+}
+​
+function createKafkaConfig(connectionOptions: ConnectionOptions): KafkaConfig {
+	return {
+		clientId: "vscode-kafka",
+		brokers: connectionOptions.bootstrap.split(","),
+		ssl: true,
+		sasl: {
+			mechanism: 'oauthbearer',
+			oauthBearerProvider: async () => {
+				const session = await vscode.authentication.getSession('redhat-mas-account-auth', ['openid'], { createIfNone: true });
+				const token = session?.accessToken!;
+				return {
+					value: token
+				};
+			}
+		}
+	};
+}
+​
 async function getRHOSAKClusters(token: string): Promise<Cluster[]> {
 	let requestConfig = {
 		params: {
 			orderBy: 'name asc'
 		},
 		headers: {
+			// eslint-disable-next-line @typescript-eslint/naming-convention
 			'Authorization': `Bearer ${token}`,
+			// eslint-disable-next-line @typescript-eslint/naming-convention
 			'Accept': 'application/json'
 		}
-	}
+	};
 	const clusters: Cluster[] = [];
 	try {
 		const response = await axios.get(KAFKA_API, requestConfig);
 		const kafkas = response.data;
 		if (kafkas && kafkas.items && kafkas.items.length > 0) {
-			kafkas.items.forEach((cluster: { id:string; status: string; name: string; bootstrapServerHost: any;  }) => {
+			kafkas.items.forEach((cluster: { id: string; status: string; name: string; bootstrapServerHost: any; }) => {
 				if (cluster?.status === 'ready') {
 					clusters.push({
 						id: cluster.id,
@@ -105,11 +114,11 @@ async function getRHOSAKClusters(token: string): Promise<Cluster[]> {
 			});
 		}
 	} catch (err) {
-		vscode.window.showErrorMessage(`Failed to load ${RHOSAK_LABEL} clusters: ${err.message}`)
+		vscode.window.showErrorMessage(`Failed to load ${RHOSAK_LABEL} clusters: ${err.message}`);
 		throw err;
 	}
 	return clusters;
 }
-
+​
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
--- src/vscodekafka-api.ts
+++ src/vscodekafka-api.ts
@@ -2,40 +2,48 @@ export interface ConnectionOptions {
     clusterProviderId?: string;
     bootstrap: string;
 }
-
+​
 export interface ClusterIdentifier {
     clusterProviderId?: string;
     id: string;
     name: string;
 }
-
+​
 export interface Cluster extends ClusterIdentifier, ConnectionOptions {
 }
-
+​
 export interface ClusterSettings {
     getAll(): { bootstrap: string }[]
 }
-
-export interface ClusterProviderProcessor {
-
+​
+export interface KafkaExtensionParticipant {
+​
+    getClusterProviderParticipant(clusterProviderId: string) : ClusterProviderParticipant;
+​
+}
+​
+/**
+ * The kafka extension participant.
+ */
+export interface ClusterProviderParticipant {
+​
     /**
-     * Returns the clusters managed by the processor which must be added to the kafka explorer.
+     * Returns the Kafka clusters managed by this participant.
      *
-     * @param clusterSettings the cluster settings.
+     * @param clusterSettings the current cluster settings.
      */
-    collectClusters(clusterSettings: ClusterSettings): Promise<Cluster[] | undefined>;
-
+    configureClusters(clusterSettings: ClusterSettings): Promise<Cluster[] | undefined>;
+​
     /**
-     * Create the Kafka JS client configuration from the given connection options.
-     * When processor doesn't implement this method, the Kafka JS client
-     * configuration is created with the default client configuration factory from the vscode-kafka.
+     * Create the KafkaJS client configuration from the given connection options.
+     * When the participant doesn't implement this method, the KafkaJS client
+     * configuration is created with the default client configuration factory from vscode-kafka.
      *
-     * @param connectionOptions the connection options.
+     * @param connectionOptions the Kafka connection options.
      */
     createKafkaConfig?(connectionOptions: ConnectionOptions): KafkaConfig;
 }
-
-
+​
 // Simplified for RHOSAK, from https://github.com/tulios/kafkajs/blob/0ed42b17bdc8786f8676f43b5ac5a79a7f0e42b7/types/index.d.ts#L19-L33
 export interface KafkaConfig {
     brokers: string[]
@@ -48,19 +56,19 @@ export interface KafkaConfig {
     requestTimeout?: number
     enforceRequestTimeout?: boolean
 }
-
+​
 export type SASLMechanismOptionsMap = {
     'plain': { username: string, password: string },
     'scram-sha-256': { username: string, password: string },
     'scram-sha-512': { username: string, password: string },
     'aws': { authorizationIdentity: string, accessKeyId: string, secretAccessKey: string, sessionToken?: string },
     'oauthbearer': { oauthBearerProvider: () => Promise<OauthbearerProviderResponse> }
-}
-
-export type SASLMechanism = keyof SASLMechanismOptionsMap
-type SASLMechanismOptions<T> = T extends SASLMechanism ? { mechanism: T } & SASLMechanismOptionsMap[T] : never
-export type SASLOptions = SASLMechanismOptions<SASLMechanism>
-
+};
+​
+export type SASLMechanism = keyof SASLMechanismOptionsMap;
+type SASLMechanismOptions<T> = T extends SASLMechanism ? { mechanism: T } & SASLMechanismOptionsMap[T] : never;
+export type SASLOptions = SASLMechanismOptions<SASLMechanism>;
+​
 export interface OauthbearerProviderResponse {
     value: string
 }
